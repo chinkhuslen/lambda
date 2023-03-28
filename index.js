25d63186ef39340db7b2207467d27d7b2b0e0587
chinkhuslen <chinkhuslen10@gmail.com>
2023-03-28 12:02:15 +0800

    dynamo db:

--- index.js
+++ index.js
@@ -1,60 +1,53 @@
-// const AWS = require("aws-sdk");
-// const lambda = new AWS.Lambda();
-// const nodemailer = require("nodemailer");
-
-// module.exports.invokerFunc = async (event) => {
-//   const params = {
-//     FunctionName:
-//       "arn:aws:lambda:us-east-1:746204573428:function:serverless-test-dev-function1",
-//   };
-//   const response = await lambda.invoke(params).promise();
-//   return response;
-// };
-
-// module.exports.handler = async (event) => {
-//   let transporter = nodemailer.createTransport({
-//     host: "smtp.gmail.com",
-//     port: 587,
-//     secure: false, // true for 465, false for other ports
-//     auth: {
-//       user: "chinkhuslen99@gmail.com", // generated ethereal user
-//       pass: "ioodbtqqezwkfbwt", // generated ethereal password
-//     },
-//   });
-
-//   // send mail with defined transport object
-//   let info = await transporter.sendMail({
-//     from: "chinkhuslen99@gmail.com", // sender address
-//     to: "chinkhuslen99@gmail.com,chinkhuslen10@gmail.com", // list of receivers
-//     subject: "Hello ✔", // Subject line
-//     text: "Hello world?", // plain text body
-//     html: "<b>Hello world?</b>", // html body
-//   });
-
-//   return info.messageId;
-// };
-const AWS = require("aws-sdk");
-const lambda = new AWS.Lambda();
-const nodemailer = require("nodemailer");
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 const axios = require("axios");
-module.exports.invokerFunc = async (event) => {
-  const params = {
-    FunctionName:
-      "arn:aws:lambda:us-east-1:746204573428:function:serverless-test-dev-function1",
-    Payload: JSON.stringify(event),
-  };
-  const response = await lambda.invoke(params).promise();
-  return {
-    body: JSON.stringify({ input: event }),
-  };
-};
-
-module.exports.handler = async (event, context) => {
-  const SearchCat = event["SearchCat"];
-  console.log(SearchCat);
-  let res = await axios.get(
-    `https://api.giphy.com/v1/gifs/search?api_key=QPHq62keOwy2IJ46dWicOPFANBwsBnK4&limit=50&offset=0&q=${SearchCat}`
+const db = new DynamoDB();
+const bcrypt = require("bcrypt");
+const helper = password;
+exports.handlerPut = async (event) => {
+  const data = await axios.get(
+    "https://api.giphy.com/v1/gifs/search?api_key=QPHq62keOwy2IJ46dWicOPFANBwsBnK4&limit=5&offset=0&q=batman"
   );
-  console.log(res);
-  return res;
+  const datasort = data.data?.data.map((el) => el.url);
+  const param = {
+    userId: "123",
+    gifname: datasort,
+  };
+  const response = await db.putItem({
+    TableName: "UserID",
+    // Item: marshall(param),
+    Item: marshall({
+      userId: "123",
+      userName: "username",
+      email: "email@email.com",
+      password: "",
+    }),
+  });
+  return response;
+};
+exports.handlerGet = async () => {
+  const response = await db.getItem({
+    TableName: "UserID",
+    Key: marshall({ userId: "aaa" }),
+  });
+  return unmarshall(response.Item);
+};
+exports.handlerUpdate = async () => {
+  const data = await axios.get(
+    "https://api.giphy.com/v1/gifs/search?api_key=QPHq62keOwy2IJ46dWicOPFANBwsBnK4&limit=5&offset=0&q=superman"
+  );
+  const datasort = data.data?.data.map((el) => el.url);
+  const response = await db.updateItem({
+    TableName: "UserID",
+    Key: marshall({ userId: "aaa" }),
+    UpdateExpression: "set gifname = :superman",
+    ExpressionAttributeValues: {
+      ":superman": {
+        L: datasort.map((el) => {
+          return { S: el };
+        }),
+      },
+    },
+  });
+  return response;
 };
