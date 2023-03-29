cd9be64427c2cbeab23e96be30b49cdfc99d4238
chinkhuslen <chinkhuslen10@gmail.com>
2023-03-29 11:44:55 +0800

    dynamodb crud

--- index.js
+++ index.js
@@ -1,51 +1,53 @@
 const { DynamoDB } = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
-const axios = require("axios");
+
+// const axios = require("axios");
+// const bcrypt = require("bcryptjs");
 const db = new DynamoDB();
-const bcrypt = require("bcrypt");
-const helper = password;
+
 exports.handlerPut = async (event) => {
-  const data = await axios.get(
-    "https://api.giphy.com/v1/gifs/search?api_key=QPHq62keOwy2IJ46dWicOPFANBwsBnK4&limit=5&offset=0&q=batman"
-  );
-  const datasort = data.data?.data.map((el) => el.url);
-  const param = {
-    userId: "123",
-    gifname: datasort,
-  };
+  //   const data = await axios.get(
+  //     "https://api.giphy.com/v1/gifs/search?api_key=QPHq62keOwy2IJ46dWicOPFANBwsBnK4&limit=5&offset=0&q=batman"
+  //   );
+  //   const datasort = data.data?.data.map((el) => el.url);
+  //   const param = {
+  //     userId: "123",
+  //     gifname: datasort,
+  //   };
   const response = await db.putItem({
     TableName: "UserID",
     // Item: marshall(param),
     Item: marshall({
-      userId: "123",
+      name: "bbb",
+      uid: "123",
       userName: "username",
       email: "email@email.com",
-      password: "",
+      password: "123456",
     }),
   });
   return response;
 };
+
 exports.handlerGet = async () => {
   const response = await db.getItem({
     TableName: "UserID",
-    Key: marshall({ userId: "aaa" }),
+    Key: marshall({ name: "aaa" }),
   });
   return unmarshall(response.Item);
 };
+
 exports.handlerUpdate = async () => {
-  const data = await axios.get(
-    "https://api.giphy.com/v1/gifs/search?api_key=QPHq62keOwy2IJ46dWicOPFANBwsBnK4&limit=5&offset=0&q=superman"
-  );
-  const datasort = data.data?.data.map((el) => el.url);
+  //   const data = await axios.get(
+  //     "https://api.giphy.com/v1/gifs/search?api_key=QPHq62keOwy2IJ46dWicOPFANBwsBnK4&limit=5&offset=0&q=superman"
+  //   );
+  //   const datasort = data.data?.data.map((el) => el.url);
   const response = await db.updateItem({
     TableName: "UserID",
-    Key: marshall({ userId: "aaa" }),
-    UpdateExpression: "set gifname = :superman",
+    Key: marshall({ name: "aaa" }),
+    UpdateExpression: "set uid = :p",
     ExpressionAttributeValues: {
-      ":superman": {
-        L: datasort.map((el) => {
-          return { S: el };
-        }),
+      ":p": {
+        S: "asd60",
       },
     },
   });
