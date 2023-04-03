91b26ec1266e9cdf1794648bc2d8d9eec26a6f99
chinkhuslen <chinkhuslen10@gmail.com>
2023-04-03 11:43:28 +0800

    S3

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { DynamoDB } = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
-
+const { PutObjectCommand, S3Client } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { S3 } = require("aws-sdk");
+const s3 = new S3();
 // const axios = require("axios");
 // const bcrypt = require("bcryptjs");
 const db = new DynamoDB();
@@ -52,4 +55,65 @@ exports.handlerUpdate = async () => {
     },
   });
   return response;
+};
+// function put(url, data) {
+//   return new Promise((resolve, reject) => {
+//     const req = https.request(
+//       url,
+//       { method: "PUT", headers: { "Content-Length": new Blob([data]).size } },
+//       (res) => {
+//         let responseBody = "";
+//         res.on("data", (chunk) => {
+//           responseBody += chunk;
+//         });
+//         res.on("end", () => {
+//           resolve(responseBody);
+//         });
+//       }
+//     );
+//     req.on("error", (err) => {
+//       reject(err);
+//     });
+//     req.write(data);
+//     req.end();
+//   });
+// }
+
+// exports.ChBucket = async () => {
+//   const REGION = "us-east-1";
+//   const BUCKET = "ch-bucket-yaml";
+//   const KEY = "img.png";
+//   let url = null;
+//   const createPresignedUrlWithClient = async ({ region, bucket, key }) => {
+//     const client = new S3Client({ region });
+//     const command = new PutObjectCommand({ Bucket: bucket, Key: key });
+//     return getSignedUrl(client, command, { expiresIn: 3600 });
+//   };
+
+//   try {
+//     const clientUrl = await createPresignedUrlWithClient({
+//       region: REGION,
+//       bucket: BUCKET,
+//       key: KEY,
+//     });
+//     url = clientUrl;
+//     console.log("Calling PUT using presigned URL with client");
+//     // await put(clientUrl, "Hello World");
+//     console.log(clientUrl);
+//     console.log("\nDone. Check your S3 console.");
+//   } catch (err) {
+//     console.error(err);
+//   }
+//   return url;
+// };
+
+exports.ChBucket = async () => {
+  const params = {
+    Bucket: "ch-bucket-yaml",
+    Key: "img.png",
+    ContentType: "image/png",
+  };
+  let url = s3.getSignedUrl("putObject", params);
+  console.log(url);
+  return url;
 };
